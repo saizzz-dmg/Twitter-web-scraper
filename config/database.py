3476b6141f881f48a715b4fb02dda5a19c7cbc4b
Sairam R Sureshbabu <165698094+saizzz-dmg@users.noreply.github.com>
2024-11-17 15:16:32 +0530

    comments for creating client

--- config/database.py
+++ config/database.py
@@ -1,5 +1,6 @@
 from pymongo.mongo_client import MongoClient
 
+#in place of username and password , plase feed the user name and passowrd of your mongodb cluster
 client = MongoClient("mongodb+srv://<username>:<password>@cluster01.7snapwc.mongodb.net/?retryWrites=true&w=majority&appName=cluster01")
 
 db = client.trendingdb
